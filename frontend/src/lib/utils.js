efcc16e4874c00539bcda88c434b6931d0763179
Editzuu07 <edyroxana96@gmail.com>
2026-08-17 12:00:17 +0300

    remade frontend

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,8 @@
-import { clsx } from "clsx";
+import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Merges Tailwind class strings, letting later classes win over
+// earlier conflicting ones (e.g. cn("p-2", "p-4") -> "p-4").
 export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
